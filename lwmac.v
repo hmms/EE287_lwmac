75e04e5bc15be366356d3ebc484f37f190386f5d
Muralidhar Shenoy <murlidhar.shenoy@gmail.com>
2013-11-06 21:56:32 -0800

    added  linear feedback shift register

--- lwmac.v
+++ lwmac.v
@@ -1,4 +1,14 @@
 module lwmac(clk,reset,busWrite,busDataToLmac,busDataFromLmac,busAddr,pushSym,Sym,pushOut,dataOut,macSate);
-input
+input clk;
+input reset;
+input busWrite;
+input [31:0]busDataToLmac;
+output [31:0] busDataFromLmac;
+input [31:0] busAddr;
+input pushSym;
+input [4:0]Sym;
+output pushOut;
+output [8:0] dataOut;
+output [3:0] macState;
 
 endmodule
